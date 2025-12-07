0ce28353a430217b6e462e90f56894e146b54923
Tomasz Kawalec <tkawalec369@gmail.com>
2025-12-07 18:55:07 +0100

    Add dry run mode

--- src/main.py
+++ src/main.py
@@ -8,12 +8,15 @@ import managed_files
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('root', action='store', nargs=1)
+    parser.add_argument('root', action='store', nargs=1, help="the root TOML configuration file")
+    parser.add_argument('--dry-run', '-d', action='store_true', help="don't perform any actual operations on the filesystem")
     parse_result = parser.parse_args()
     # parser.print_help()
     # print(parse_result)
 
     conf_root_path = parse_result.root[0]
+    dry_run = parse_result.dry_run
+    print(f"{dry_run=}")
     conf = conf_reader.Conf.read(conf_root_path)
     # print(conf)
 
@@ -30,17 +33,19 @@ def main():
     dir_working = pathlib.Path("./working").resolve()
     for gn in enabled_groups:
         g = conf.get_group(gn)
-        renderer.render_group(conf, g, managed_new_list, dir_working)
+        renderer.render_group(conf, g, managed_new_list, dir_working, dry_run=dry_run)
 
     managed_deleted_list = list(set(managed_old_list) - set(managed_new_list))
     if len(managed_deleted_list) > 0:
         print(f"The following files were managed but are to be deleted: {', '.join(managed_deleted_list)}")
-        for mp in managed_deleted_list:
-            os.unlink(mp)
+        if not dry_run:
+            for mp in managed_deleted_list:
+                os.unlink(mp)
     else:
         print("There are no previously managed files to be deleted")
 
-    managed_files.save_managed_files(conf_root_path, managed_new_list)
+    if not dry_run:
+        managed_files.save_managed_files(conf_root_path, managed_new_list)
 
 if __name__ == "__main__":
     main()
--- src/renderer.py
+++ src/renderer.py
@@ -13,27 +13,33 @@ def render_file(
         conf: conf_reader.Conf,
         group: conf_reader.Group,
         managed_new_list: typing.List[str],
-        use_symlinks: bool):
+        use_symlinks: bool,
+        dry_run: bool):
     print(f"Running processor {group.process} on {in_file_path} into {path_working}")
-    processing.process_dict[group.process](in_file_path, path_working, conf)
+    if not dry_run:
+        processing.process_dict[group.process](in_file_path, path_working, conf)
 
     symlink_path = group.out_path / in_file_path.relative_to(group.in_path)
-    symlink_path.parent.mkdir(parents=True, exist_ok=True)
-    symlink_path.unlink(missing_ok=True)
+    if not dry_run:
+        symlink_path.parent.mkdir(parents=True, exist_ok=True)
+        symlink_path.unlink(missing_ok=True)
     managed_new_list.append(str(symlink_path))
     if use_symlinks:
         print(f"Creating symlink {symlink_path} -> {path_working}")
-        os.symlink(path_working, symlink_path)
+        if not dry_run:
+            os.symlink(path_working, symlink_path)
     else:
         print(f"Copying {symlink_path} -> {path_working}")
-        shutil.copy(path_working, symlink_path)
+        if not dry_run:
+            shutil.copy(path_working, symlink_path)
 
 def render_group(
         conf: conf_reader.Conf,
         group: conf_reader.Group,
         managed_new_list: typing.List[str],
         dir_working: pathlib.Path,
-        use_symlinks=True):
+        use_symlinks=True,
+        dry_run=False):
     dir_working_group = dir_working / group.name
     dir_working_group.mkdir(parents=True, exist_ok=True)
     in_path_obj = pathlib.Path(group.in_path)
@@ -41,9 +47,9 @@ def render_group(
         for (dir_path, dirs, files) in in_path_obj.walk(follow_symlinks=True):
             for fn in files:
                 path_working = dir_working_group / f"{fn}_{random.randint(0, 9999) :0>4}"
-                render_file(dir_path / fn, path_working, conf, group, managed_new_list, use_symlinks)
+                render_file(dir_path / fn, path_working, conf, group, managed_new_list, use_symlinks, dry_run)
     elif in_path_obj.is_file():
         path_working = dir_working_group / f"{in_path_obj.name}_{random.randint(0, 9999) :0>4}"
-        render_file(in_path_obj, path_working, conf, group, managed_new_list, use_symlinks)
+        render_file(in_path_obj, path_working, conf, group, managed_new_list, use_symlinks, dry_run)
     else:
         print(f"Warning: path {in_path_obj} does not exist")
